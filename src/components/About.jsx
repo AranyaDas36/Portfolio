c0ebd4295a096e429c3d80fe758bf8dc5a51b834
Aranya Das <aranyadas@Aranyas-MacBook-Air.local>
2025-04-30 14:55:53 +0530

    new changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,30 +7,30 @@ const About = () => {
         About <span className='text-neutral-500'>Me</span>
       </h1>
 
-      <div className='space-y-8'>
+      <div className='space-y-9'>
         <div>
           <h2 className='text-3xl bg-white-300 font-bold inline-block mb-3'>Languages: </h2>
-          <div className='flex flex-wrap gap-3'>
+          <div className='flex flex-wrap gap-4'>
             {['C', 'C++', 'JavaScript', 'Typescript'].map((lang) => (
-              <span key={lang} className='text-white font-bold border px-3 py-1 rounded'>{lang}</span>
+              <span key={lang} className='text-black bg-gray-100 font-bold border px-3 py-1 rounded'>{lang}</span>
             ))}
           </div>
         </div>
 
         <div>
           <h2 className='text-3xl bg-white-300 font-bold inline-block mb-3'>Developer Tools: </h2>
-          <div className='flex flex-wrap gap-3'>
+          <div className='flex flex-wrap gap-4'>
             {['Git', 'GitHub'].map((tool) => (
-              <span key={tool} className='text-white font-bold border px-3 py-1 rounded'>{tool}</span>
+              <span key={tool} className='text-neutral-800 bg-gray-100 font-bold border px-3 py-1 rounded'>{tool}</span>
             ))}
           </div>
         </div>
 
         <div>
           <h2 className='text-3xl bg-white-300 font-bold inline-block mb-3'>Technical Skilla: </h2>
-          <div className='flex flex-wrap gap-3'>
+          <div className='flex flex-wrap gap-4'>
             {['ReactJS', 'TailwindCSS', 'Node.js', 'ExpressJS', 'MongoDB', 'PostgreSQL', 'Prisma', 'Hono', 'Serverless Backend', 'AWS'].map((tool) => (
-              <span key={tool} className='text-white font-bold border px-3 py-1 rounded'>{tool}</span>
+              <span key={tool} className='text-black bg-gray-100 font-bold border px-3 py-1 rounded'>{tool}</span>
             ))}
           </div>
         </div>
